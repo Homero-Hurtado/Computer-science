998ef7336aa3a3a61f2bb6518619102b94570240
Homero Hurtado <homerohg28@gmail.com>
2022-06-11 20:39:26 -0500

    feat: reestructura de codigo factorial

--- 1. Lógica de programación/1.4-Code/factorial.js
+++ 1. Lógica de programación/1.4-Code/factorial.js
@@ -2,14 +2,10 @@ var numero = window.prompt("¿De qué valor necesitas el factorial?");
 numero = parseInt(numero);
 var factorial = 1;
 
-if(numero === 0 || numero ===1){
-    factorial = 1;
-    }else{
-    console.log('Use While');
-    while(numero !=0){
+    while(numero !=0 && numero !==1){
+    console.log('use while');
     factorial = factorial * numero;
     numero--;
-}
 }
 console.log("El resultado del factorial es:",factorial)
 
