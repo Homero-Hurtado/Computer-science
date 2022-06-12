f1227e13a68edbc9bf0411274fabd74f8002e6ff
Homero Hurtado <homerohg28@gmail.com>
2022-06-12 12:25:09 -0500

    feat: uso de debugger

--- 1. Lógica de programación/1.4-Code/matrices/matrices.js
+++ 1. Lógica de programación/1.4-Code/matrices/matrices.js
@@ -32,19 +32,19 @@ var matrizE = [
 for(var i = 0; i < matrizA.length; i++){
     for(var j = 0; j < matrizA.length; j++){
     console.log("matrizA: ", matrizA[i][j]);
-    // i=0, j=0, j (0) < matrizA.length (3) true
-    //      j=1, j (1) < matrizA.length (3) true 
-    //      j=2, j (2) < matrizA.length (3) true
-    //      j=3, j (3)< matrizA.length (3) false
-    // i=1, j=0, j (0) < matrizA.length (3) true
-    //      j=1, j (1) < matrizA.length (3) true 
-    //      j=2, j (2) < matrizA.length (3) true
-    //      j=3, j (3)< matrizA.length (3) false
+//     // i=0, j=0, j (0) < matrizA.length (3) true
+//     //      j=1, j (1) < matrizA.length (3) true 
+//     //      j=2, j (2) < matrizA.length (3) true
+//     //      j=3, j (3)< matrizA.length (3) false
+//     // i=1, j=0, j (0) < matrizA.length (3) true
+//     //      j=1, j (1) < matrizA.length (3) true 
+//     //      j=2, j (2) < matrizA.length (3) true
+//     //      j=3, j (3)< matrizA.length (3) false
     
-    // i=2, j=0, j (0) < matrizA.length (3) true
-    //      j=1, j (1) < matrizA.length (3) true 
-    //      j=2, j (2) < matrizA.length (3) true
-    //      j=3, j (3)< matrizA.length (3) false
+//     // i=2, j=0, j (0) < matrizA.length (3) true
+//     //      j=1, j (1) < matrizA.length (3) true 
+//     //      j=2, j (2) < matrizA.length (3) true
+//     //      j=3, j (3)< matrizA.length (3) false
 }
 // i++
 }
@@ -52,6 +52,9 @@ for(var i = 0; i < matrizA.length; i++){
 //SUMA
 console.log("matrizA: ", matrizA);
 console.log("matrizB: ", matrizB);
+
+//debugger 
+
 for(var i = 0; i < matrizA.length; i++){
     for(var j = 0; j < matrizA.length; j++){
     matrizC[i][j] = matrizA[i][j] + matrizB[i][j];
@@ -60,7 +63,7 @@ for(var i = 0; i < matrizA.length; i++){
 }
 console.log("matrizC: (suma)", matrizC);
 
-//Resta
+// //Resta
 
 console.log("matrizA: ", matrizA);
 console.log("matrizB: ", matrizB);
