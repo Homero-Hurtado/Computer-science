2dc464f70ffea98895f53ebd781cc4f70137bd5d
Homero Hurtado <homerohg28@gmail.com>
2022-06-16 14:59:04 -0500

    feat: decalarativo e imperativo, operadores en Js

---
+++ 2. EcmaScript/notas/operadores.js
@@ -0,0 +1,26 @@
+// Operadores aritmeticos
+// * / + -
+
+//     operadores relacionales/logicos
+//     || && !
+
+//     incremento / decremento
+//     num++ num-- ++num --num
+
+let numero = 10;
+let numero2 = numero++;  // numero2 = 10 // numero = 11 
+let numero3 = ++numero; // numero3 = 11 // numero = 11
+console.log(numero, numero2, numero3);
+
+//operadores de asignacion compuestos
+// -= +=
+
+//operadorres de comparacion
+// ===
+// !==
+// ==
+// !=
+// <
+// >
+//<=
+// >=
