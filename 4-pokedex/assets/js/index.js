1698ca5c807aa5b09f7dc3b180fe7722b48ca204
Homero Hurtado <homerohg28@gmail.com>
2022-06-23 00:38:27 -0500

    fix: pintado de elementos pokemons

---
+++ 4-pokedex/assets/js/index.js
@@ -0,0 +1,62 @@
+const NOT_IMAGE_TEXT = 'la imagen del pokemon';
+let pokemons = [];
+
+const getPokemons = async () => {
+    console.log('paso 2');
+    //fetch(‘https://pokeapi.co/api/v2/pokemon/ditto’)
+    //.then(response => response.json())
+    //.then(data => console.log(data));
+    const response = await fetch('https://pokeapi.co/api/v2/pokemon/');
+    const responseJson = await response.json();
+    const pokemons = responseJson.results;
+    for (const element of pokemons) {
+        const response = await fetch(element.url);
+        const imgResponseJson = await response.json();
+        normalizePokemonData(element.name, imgResponseJson)
+    };
+};
+
+const normalizePokemonData = (name, imgResponseJson) => {
+    const img = imgResponseJson?.sprites?.other['oficial-artwork']?.front_default || '';
+    const pokemon = {name: name, img: img};
+    pokemons.push(pokemon);
+};
+
+const renderCardPokemon = (element, index) => {
+
+    console.log('element: ', element, index);
+
+    const cardPokemonDiv = document.createElement('div');
+    const pokemonImg = document.createElement('img');
+    const brElement = document.createElement('br');
+    const pokemonNameSpan = document.createElement('span');
+
+    cardPokemonDiv.className = 'venter';
+    pokemonImg.className = 'icon-region';
+    pokemonImg.setAttribute('src', element.img);
+    pokemonImg.setAttribute('alt', NOT_IMAGE_TEXT);
+
+    const mainDiv = document.querySelector('#container-pokemons');
+    mainDiv.appendChild(cardPokemonDiv);
+    
+    cardPokemonDiv.appendChild(pokemonImg);
+    cardPokemonDiv.appendChild(brElement);
+    cardPokemonDiv.appendChild(pokemonNameSpan);
+    pokemonNameSpan.innerHTML = element.name;
+
+}
+
+const renderPokemons = (pokemons) => {
+    console.log('5 pokemons =>', pokemons );
+    pokemons.forEach(renderCardPokemon);
+}
+
+async function main(){
+    console.log('paso 1');
+    await getPokemons();
+    console.log('paso 3');
+    console.log('4 pokemons =>', pokemons);
+    renderPokemons(pokemons);
+    console.log('paso 6');
+}
+main();
