95b7548f0fc62e336596f3efd326084fe4f28ee6
Homero Hurtado <homerohg28@gmail.com>
2022-06-24 15:29:51 -0500

    feat: ejemplos de objetos literales

---
+++ 6-prototype-poo/2-objetos-literales.js
@@ -0,0 +1,12 @@
+var flash = {
+    superpoder: "suepervelocidad",
+    tipo: "superheroes",
+    alias: "flash"
+};
+
+var mujerMaravilla = {
+    superpoder: "superfuerza",
+    tipo: "superheroe",
+    alias: "mujer maravilla"
+};
+
